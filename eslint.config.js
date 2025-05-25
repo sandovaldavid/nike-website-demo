e54645d5a94b640b029bf40a98cdc3d4765452ea
sandovaldavid <sandovaldavid2201@gmail.com>
2025-05-25 16:33:03 -0500

    🔧    Add ESLint configuration file to enforce coding standards and integrate Prettier for consistent formatting

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+    {
+        ignores: ['.config/', 'dist/', 'tsconfig.json', 'node_modules/', '.astro/'],
+    },
+    { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+    tseslint.configs.recommended,
+    {
+        files: ['**/*.json'],
+        plugins: { json },
+        language: 'json/json',
+        extends: ['json/recommended'],
+    },
+    {
+        files: ['**/*.md'],
+        plugins: { markdown },
+        language: 'markdown/gfm',
+        extends: ['markdown/recommended'],
+    },
+    { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: {
+            prettier,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+        },
+    },
+    eslintConfigPrettier,
+]);
