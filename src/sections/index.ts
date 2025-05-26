ec59c253f50a7e3eb67e128f5617db96fa67715b
sandovaldavid <sandovaldavid2201@gmail.com>
2025-05-26 00:10:48 -0500

    ✨   Create index.ts to export all section components for improved modularity and organization.

---
+++ src/sections/index.ts
@@ -0,0 +1,8 @@
+export { default as Hero } from './Hero.astro';
+export { default as PopularProducts } from './PopularProducts.astro';
+export { default as SuperQuality } from './SuperQuality.astro';
+export { default as Services } from './Services.astro';
+export { default as SpecialOffers } from './SpecialOffers.astro';
+export { default as CustomerReviews } from './CustomerReviews.astro';
+export { default as Subscribe } from './Subscribe.astro';
+export { default as Footer } from './Footer.astro';
